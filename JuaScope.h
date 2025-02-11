077bc67ad05e71484a7260935491028aadc612b7
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-02-11 22:42:52 +0330

    Native Jua functions special addrs added

--- JuaScope.h
+++ JuaScope.h
@@ -14,7 +14,7 @@ enum JuaScopeStatus {
 
 struct JuaScopeRes {
 	JuaScopeStatus status;
-	size_t addr;
+	std::string addr;
 	std::string ident_name;
 };
 
@@ -29,9 +29,22 @@ class JuaScope {
 	std::vector<JuaAbstractScope> scopes;
 
 	std::vector<size_t> removed_addrs;
+
+	bool func_style = false;
+	size_t func_style_start_ind = 0;
 public:
 	JuaScope() {
 		addr_counter = 0;
+		func_style = false;
+	}
+
+	void activate_func_style() {
+		func_style = true;
+		func_style_start_ind = addr_counter;
+	}
+
+	void deactivate_func_style() {
+		func_style = false;
 	}
 
 	void create_new_scope() {
@@ -45,14 +58,23 @@ public:
 		}
 
 		for (auto i : scopes.back().temp_addrs) {
-
+			
 			removed_addrs.push_back(i);
 		}
 
 		scopes.pop_back();
 	}
 
-	JuaScopeRes get_ident_name(size_t addr) {
+	JuaScopeRes get_ident_name(std::string addr_s) {
+		size_t addr;
+
+		if (addr_s[0] == '$') {
+			addr = std::stoull(addr_s.substr(1).c_str()) + func_style_start_ind;
+		}
+		else {
+			addr = std::stoull(addr_s);
+		}
+
 		for (auto& scope : scopes) {
 
 			auto& main_vars = scope.main_vars;
@@ -62,12 +84,12 @@ public:
 					main_vars.erase(elm.first);
 					removed_addrs.push_back(elm.second);
 
-					return { JuaScopeStatus::JSCOPE_SUCCESS, elm.second, elm.first };
+					return { JuaScopeStatus::JSCOPE_SUCCESS, std::to_string(elm.second), elm.first };
 				}
 			}
 		}
 
-		return { JuaScopeStatus::JSCOPE_NOT_FOUND , 0 , "" };
+		return { JuaScopeStatus::JSCOPE_NOT_FOUND , "0" , "" };
 	}
 
 	JuaScopeRes get_new_tmp() {
@@ -77,21 +99,37 @@ public:
 			removed_addrs.pop_back();
 			scopes.back().temp_addrs.push_back(addr);
 
-			return{ JuaScopeStatus::JSCOPE_SUCCESS , addr , "" };
+			return{ JuaScopeStatus::JSCOPE_SUCCESS , std::to_string(addr) , "" };
 		}
 
-		size_t addr = addr_counter++;
-		scopes.back().temp_addrs.push_back(addr);
+		size_t addr_res = addr_counter++;
+		scopes.back().temp_addrs.push_back(addr_res);
+
+		std::string addr = std::to_string(addr_res);
+
+		if (func_style) {
+			addr = "$" + std::to_string(addr_res - func_style_start_ind);
+		}
 
 		return { JuaScopeStatus::JSCOPE_SUCCESS , addr , "" };
 	}
 
 	JuaScopeRes get_new_addr(const std::string& var) {
 		for (auto scope : scopes) {
+
 			auto& main_vars = scope.main_vars;
 
+			if (func_style) {
+				if (scope.main_vars == scopes.back().main_vars) {
+
+					if (main_vars.find(var) != main_vars.end()) {
+						return { JuaScopeStatus::JSCOPE_SUCCESS ,"$" + std::to_string(main_vars[var]) , ""};
+					}
+				}
+			}
+
 			if (main_vars.find(var) != main_vars.end()) {
-				return { JuaScopeStatus::JSCOPE_SUCCESS ,main_vars[var] , "" };
+				return { JuaScopeStatus::JSCOPE_SUCCESS ,std::to_string(main_vars[var]) , "" };
 			}
 		}
 
@@ -103,9 +141,16 @@ public:
 		else {
 			scopes.back().main_vars[var] = addr_counter++;
 		}
+		
+		size_t addr_res = scopes.back().main_vars[var];
+		std::string addr = std::to_string(scopes.back().main_vars[var]);
+
+		if (func_style) {
+			addr = "$" + std::to_string(addr_res - func_style_start_ind);
+		}
 
 
-		return { JuaScopeStatus::JSCOPE_SUCCESS , scopes.back().main_vars[var] , ""};
+		return { JuaScopeStatus::JSCOPE_SUCCESS , addr , ""};
 	}
 
 	//JuaScopeRes is_exists(const std::string& var) {
