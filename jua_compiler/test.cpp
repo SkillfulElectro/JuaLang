3db6a9a0838661eafd23569efd1da029aa1c09dd
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-02-13 15:31:07 +0330

    test example updated

--- jua_compiler/test.cpp
+++ jua_compiler/test.cpp
@@ -6,13 +6,42 @@ int main() {
 	JuaLang instance;
 	std::cout << instance.compile(
 		R"(
-hi = 1;
-while(hi)
+hi = 3;
+if(hi - 2)
 { 
-	hi = hi + 1;
-	print(hi);
-	continue;
-} 
+	print("if state");
+	while (hi) {
+		hi = hi - 1;
+		print(hi);
+		if (hi - 1) {
+			print("test");
+			break;
+		}
+	}
+} else if ( hi - 1 ) {
+	print("else if state");
+
+	while (hi) {
+		hi = hi - 1;
+		print(hi);
+		if (hi - 1) {
+			print("test");
+			break;
+		}
+	}
+} else {
+	print("else state");
+
+	while (hi + 1) {
+		hi = hi - 1;
+		print(hi);
+		if (hi + 2) {
+			print("test");
+			continue;
+		}
+	}
+}
+
 return hi;
 )");
 
