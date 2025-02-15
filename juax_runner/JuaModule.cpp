2b65446fbea5c57623773b34256e9f2bda142b4d
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-02-15 11:59:04 +0330

    Delete juax_runner directory

--- juax_runner/JuaModule.cpp
+++
@@ -1,7 +0,0 @@
-// JuaModule.cpp
-#include "JuaModule.h"
-#include "JuaInterpter.h" 
-
-JuaModule::JuaModule(const std::string& func_name, JuaInterpter* instance) {
-    instance->add_extension(func_name, this);
-}
