51f18980bfd886484270c4fb0c88e66a61a1104a
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-07-26 11:42:52 +0330

    hashmap impl

--- std/structures/hashmap.h
+++ std/structures/hashmap.h
@@ -1,31 +1,153 @@
 #ifndef JUA_STD_HASHMAP
 #define JUA_STD_HASHMAP
 
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <cstdlib>
 #include "JuaLang.h"
 
 
-/////// TODO !
+namespace std {
+    template<>
+    struct hash<JuaOprand> {
+        size_t operator()(const JuaOprand& s) const noexcept {
+            switch (s.op_type) {
+            case STRING: {
 
-namespace std
-{
-    struct hash<JuaOprand>
-    {
-        size_t operator()(JuaOprand& s) const noexcept
-        {
-            ;
+                const std::string& tmp =
+                    const_cast<JuaOprand&>(s).get_str();
+                return std::hash<std::string>{}(tmp);
+            }
+
+            case DOUBLE: {
+                double tmp =
+                    const_cast<JuaOprand&>(s).get_doub();
+                return std::hash<double>{}(tmp);
+            }
+
+            case VOID: {
+                JuaVoidType* ptr =
+                    const_cast<JuaOprand&>(s).get_void_ptr();
+                if (ptr->is_hashable()) {
+                    return ptr->hash_val();
+                }
+                break;
+            }
+
+            default:
+                break;
+            }
+
+            return 0;
         }
     };
 }
 
-class JuaStdHashMap : public JuaVoidType {
-public:
-    JuaOprand run_func_by_symbol(const std::string& name , std::vector<JuaStackVal>& params) override {
+namespace std {
+    template<>
+    struct equal_to<JuaOprand> {
+        bool operator()(const JuaOprand& a, const JuaOprand& b) const noexcept {
+            if (a.op_type != b.op_type) return false;
+            switch (a.op_type) {
+            case STRING:
 
+                return const_cast<JuaOprand&>(a).get_str() ==
+                       const_cast<JuaOprand&>(b).get_str();
+            case DOUBLE:
+                return const_cast<JuaOprand&>(a).get_doub() ==
+                       const_cast<JuaOprand&>(b).get_doub();
+            case VOID: {
+                auto pa = const_cast<JuaOprand&>(a).get_void_ptr();
+                auto pb = const_cast<JuaOprand&>(b).get_void_ptr();
+
+                if (pa == pb) return true;
+                if (pa->is_equatable()) return pa->equals(pb);
+                return false;
+            }
+            default:
+                return false;
+            }
+        }
+    };
+}
+
+
+
+class JuaStdHashMap : public JuaVoidType {
+    std::unordered_map<JuaOprand, JuaOprand> map;
+
+
+    void get_oprand(JuaStackVal& stack_val, JuaOprand*& out_ptr, JuaOprand& tmp) {
+        if (stack_val.type == REF) {
+            out_ptr = stack_val.get_ptr();
+        } else {
+            tmp = stack_val.get_obj();
+            out_ptr = &tmp;
+        }
+    }
+
+public:
+    JuaStdHashMap(std::vector<JuaStackVal>& params) {
+        for (size_t i = 0; i + 1 < params.size(); i += 2) {
+            JuaOprand *key_ptr, *val_ptr;
+            JuaOprand tmp_key, tmp_val;
+            get_oprand(params[i], key_ptr, tmp_key);
+            get_oprand(params[i+1], val_ptr, tmp_val);
+            map[*key_ptr] = *val_ptr;
+        }
+    }
+
+    JuaOprand run_func_by_symbol(const std::string& name, std::vector<JuaStackVal>& params) override {
+        if (name == "size") {
+            return JuaOprand{DOUBLE, static_cast<double>(map.size())};
+        }
+        if (name == "empty") {
+            return JuaOprand{DOUBLE, map.empty() ? 1.0 : 0.0};
+        }
+        if (name == "clear") {
+            map.clear();
+            return JuaOprand{DOUBLE, 0.0};
+        }
+        if (name == "get") {
+            if (params.size() < 2) return JuaOprand{DOUBLE, 0.0};
+            JuaOprand* key_ptr; JuaOprand tmp_key;
+            get_oprand(params[1], key_ptr, tmp_key);
+            auto it = map.find(*key_ptr);
+            if (it != map.end()) return it->second;
+            return JuaOprand{DOUBLE, 0.0};
+        }
+        if (name == "set") {
+            if (params.size() < 3) return JuaOprand{DOUBLE, 0.0};
+            JuaOprand *key_ptr, *val_ptr;
+            JuaOprand tmp_key, tmp_val;
+            get_oprand(params[1], key_ptr, tmp_key);
+            get_oprand(params[2], val_ptr, tmp_val);
+            map[*key_ptr] = *val_ptr;
+            return JuaOprand{DOUBLE, 1.0};
+        }
+        if (name == "erase") {
+            if (params.size() < 2) return JuaOprand{DOUBLE, 0.0};
+            JuaOprand* key_ptr; JuaOprand tmp_key;
+            get_oprand(params[1], key_ptr, tmp_key);
+            size_t count = map.erase(*key_ptr);
+            return JuaOprand{DOUBLE, static_cast<double>(count)};
+        }
+        if (name == "contains") {
+            if (params.size() < 2) return JuaOprand{DOUBLE, 0.0};
+            JuaOprand* key_ptr; JuaOprand tmp_key;
+            get_oprand(params[1], key_ptr, tmp_key);
+            return JuaOprand{DOUBLE, map.count(*key_ptr) ? 1.0 : 0.0};
+        }
+        return JuaOprand{DOUBLE, 0.0};
     }
 };
 
-JuaOprand jua_create_hash_map(std::vector<JuaStackVal>& params) {
-    
+
+inline JuaOprand jua_create_hash_map(std::vector<JuaStackVal>& params) {
+    JuaOprand ret{VOID, new JuaStdHashMap(params)};
+    ret.destructor = [](JuaOprand* obj) { delete obj->get_void_ptr(); };
+    return ret;
 }
 
-#endif
+#endif // JUA_STD_HASHMAP
